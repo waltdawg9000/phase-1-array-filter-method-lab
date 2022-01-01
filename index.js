89bae7285e2cb6d54673233397920eb1cfd12a6c
Walter Morris <waltercmorris18@gmail.com>
2022-01-01 06:18:25 -0800

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, string){
+    const result = drivers.filter(driversName => driversName.toLowerCase() === string.toLowerCase())
+    return result;
+}
+
+function fuzzyMatch(drivers,string){
+    const result = drivers.filter(driversName => driversName.startsWith(string))
+    return result;
+}
+
+function matchName(drivers, string){
+    return drivers.filter(function(hometown){
+      return hometown.name === string
+    })
+  
+}
